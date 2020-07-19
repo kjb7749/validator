5eedac9e2d3f58ea877a8609d579521d197af139
David <hinorid@gmail.com>
2020-07-20 01:52:50 +0900

    후우우우

--- validator/validator.cpp
+++ validator/validator.cpp
@@ -2,11 +2,9 @@
 #include <fstream>
 #include <re2/re2.h>
 #include <Windows.h>
+#include <vector>
 
 using namespace std;
-
-
-
 
 DWORD convert_ansi_to_unicode_string(
     __out std::wstring& unicode,
@@ -220,33 +218,95 @@ auto fileReader = [](string filePath)
 };
 
 
-int main()
+auto splitString = [](wstring text, wstring spliter) -> vector<wstring>
 {
-    wstring a = TEXT("한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글");
-    wstring b = TEXT("한글");
+    vector<wstring> substrings;
+    size_t pos = 0;
+    wstring token;
+    while ((pos = text.find(spliter)) != wstring::npos)
+    {
+        token = text.substr(0, pos);
+        substrings.push_back(token);
+        text.erase(0, pos + spliter.length());
+    }
+    substrings.push_back(text);
 
-    //string a = "한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글";
-    //string b = "한글";
+    return substrings;
+};
 
-    string text;
+auto trimString = [](wstring text, wstring base, wstring target) -> wstring
+{
+    size_t index;
+    size_t length = lstrlen(base.c_str());
+
+    if (length <= 0)
+        return text;
+
+    while ((index = text.find(base)) != wstring::npos)
+    {
+        text.replace(index, length, target);
+    }
+    return text;
+};
+
+
+auto normalizer = [](wstring text) -> vector<wstring>
+{
+    text = trimString(text, TEXT("\t"), TEXT(""));
+    text = trimString(text, TEXT("\n"), TEXT(""));
+    //text = trimString(text, TEXT("/"), TEXT(""));
+    text = trimString(text, TEXT(" "), TEXT(""));
+    vector<wstring> strings = splitString(text, TEXT(","));
+    std::sort(strings.begin(), strings.end());
+    return strings;
+};
+
+int main()
+{   
+    string utf8Text;
+    wstring unicodeBaseText;
+    wstring unicodeTargetText;
     string pattern;
 
-    convert_unicode_to_utf8_string(text, a.c_str(), lstrlenW(a.c_str()));
-    convert_unicode_to_utf8_string(pattern, b.c_str(), lstrlenW(b.c_str()));
 
-    cout << RE2::PartialMatch(text, pattern) <<endl;
+    utf8Text = fileReader("testHeader.h");          //utf8 형태로 인코딩 된 파일에서 데이터를 읽는다
+    RE2::Replace(&utf8Text, "//.*", "");
+
+    convert_utf8_to_unicode_string(unicodeBaseText, utf8Text.c_str(), strlen(utf8Text.c_str()));        //unicode 문자열로 변환한다.
+
+    utf8Text.clear();
+    utf8Text = fileReader("testHeader2.h");
+    RE2::Replace(&utf8Text, "//", "");
+
+    convert_utf8_to_unicode_string(unicodeTargetText, utf8Text.c_str(), strlen(utf8Text.c_str()));        //unicode 문자열로 변환한다.
+
+    auto baseStrs = normalizer(unicodeBaseText);
+    auto targetStrs = normalizer(unicodeTargetText);
 
 
-    auto s = fileReader("test.txt");
 
-    wstring unicodeText;
-    string utf8Text;
-
-    //convert_ansi_to_unicode_string(unicodeText, s.c_str(), s.length());
-    //convert_unicode_to_utf8_string(utf8Text, unicodeText.c_str(), unicodeText.length());
-
-    cout << RE2::PartialMatch(s, pattern) << endl;
+    for (auto itr : baseStrs)
+    {
+        string token;
+        convert_unicode_to_ansi_string(token, itr.c_str(), lstrlen(itr.c_str()));
+        cout << token << endl;
+    }
 
 
-    //MessageBox(0, TEXT("내용"), TEXT("제목"), 0);
+    if (baseStrs.size() != targetStrs.size())
+    {
+        MessageBox(0, TEXT("개수가 일치하지 않습니다."), TEXT("개수 불일치"), 0);
+    }
+
+    size_t size = min(baseStrs.size(), targetStrs.size());
+
+    for(size_t index = 0; index < size; ++index)
+    {
+        if (baseStrs.at(index) != targetStrs.at(index))
+        {
+            MessageBox(0, TEXT("데이터가 밀렸습니다. 확인 부탁드려요!"), TEXT("데이터 밀림"), 0);
+        }
+    }
+
+    return 0;
 }
